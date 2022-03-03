c1812bfff45cd4bbd94346ca76efab2339648f47
Vaibhav <singhvaibhav557hr@gmail.com>
2022-03-03 21:14:03 +0530

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <h2>LAW_DA</h2>
+        <ul>
+          <li>FIND A LAWYER</li>
+          <li>LEGAL SERVICES</li>
+          <li>
+                <img src="/images/avatar.png" alt="avatar" />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
